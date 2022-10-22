95f123281056f7528a61d7a0af9df802be2ef841
Shannon Yazdani <shannonyazdani@gmail.com>
2022-10-22 20:15:30 -0300

    add redirects file

--- src/components/InventoryComponents/InventoryList/InventoryList.js
+++ src/components/InventoryComponents/InventoryList/InventoryList.js
@@ -31,7 +31,6 @@ class InventoryList extends React.Component {
     } else {
       let currentLocation = window.location.href;
       let currentId = currentLocation.slice(46, 82);
-      console.log(currentId);
       axios
         .get(`${url}warehouse/${currentId}/inventory`)
         .then((response) => {
